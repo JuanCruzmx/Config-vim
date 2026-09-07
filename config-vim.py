e2570738e3781ff8e42e4369c131d84d4e136b94
JuanCruzmx <juancruzosorio.mx@gmail.com>
2026-09-07 13:09:52 -0600

    Cofiguraciones basicas para vim

--- config-vim.py
+++ config-vim.py
@@ -1,7 +1,22 @@
-import platform, os
+from pathlib import Path
+import os
+import platform
 
 sistema = platform.system()
-ruta = os.getcwd()
+home = Path.home()
+actual = Path.cwd()
 
-print(f'sistema operativo: {sistema}')
-print(f'ruta: {ruta}')
+def plantillas():
+    plantilla = actual / "Plantillas"
+    plantilla_home = home / ".vim" / "Plantillas"
+    vimrc = actual / "Plantillas" / "vimrc"
+    vimrc_home = home / ".vimrc"
+    
+    if vimrc_home.exists() or vimrc_home.is_symlink():
+        vimrc_home.unlink()
+    
+    vimrc_home.symlink_to(vimrc)
+    print('Enlance creado')
+
+print(f'Sistema operativo: {sistema}')
+plantillas()
