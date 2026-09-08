fde3442655e1da96b2ab14c3459c78dce45738a2
JuanCruzmx <juancruzosorio.mx@gmail.com>
2026-09-08 11:24:11 -0600

    Creacion de plantillas con enlace hacia el home/.vim/

--- config-vim.py
+++ config-vim.py
@@ -3,20 +3,28 @@ import os
 import platform
 
 sistema = platform.system()
-home = Path.home()
-actual = Path.cwd()
 
 def plantillas():
+    actual = Path.cwd()
+    home = Path.home()
     plantilla = actual / "Plantillas"
     plantilla_home = home / ".vim" / "Plantillas"
     vimrc = actual / "Plantillas" / "vimrc"
     vimrc_home = home / ".vimrc"
     
-    if vimrc_home.exists() or vimrc_home.is_symlink():
-        vimrc_home.unlink()
-    
-    vimrc_home.symlink_to(vimrc)
-    print('Enlance creado')
+    if plantilla_home.exists() and plantilla_home.is_symlink():
+        print('Plantillas activas')
+    else:
+        plantilla_home.unlink(missing_ok=True)
+        plantilla_home.symlink_to(plantilla)
+        print('Plantillas creadas')
+   
+    if vimrc_home.exists() and vimrc_home.is_symlink():
+        print('Enlazado')
+    else:
+        vimrc_home.unlink(missing_ok=True)
+        vimrc_home.symlink_to(vimrc)
+        print('Enlace creado')
 
 print(f'Sistema operativo: {sistema}')
 plantillas()
