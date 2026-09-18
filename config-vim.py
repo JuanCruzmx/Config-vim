76f5e8c15afb1891bf414f3994c15acd513a4d0a
JuanCruzmx <juancruzosorio.mx@gmail.com>
2026-09-17 20:45:58 -0600

    Modificación de variables...

--- config-vim.py
+++ config-vim.py
@@ -8,22 +8,22 @@ home = Path.home()
 
 def config():
     plantilla = actual / "Plantillas"
-    vim = actual / "Plantillas" / "vimrc"
-    vim_home = home / ".vimrc"
-    plantillas(vim, plantilla)
-    comandos(vim)
+    vimrc = actual / "Plantillas" / "vimrc"
+    vimrc_home = home / ".vimrc"
+    plantillas(vimrc, plantilla)
+    comandos(vimrc)
    
-    if vim_home.exists() and vim_home.is_symlink():
+    if vimrc_home.exists() and vimrc_home.is_symlink():
         print('Enlazado')
     else:
-        vim_home.unlink(missing_ok=True)
-        vim_home.symlink_to(vim)
+        vimrc_home.unlink(missing_ok=True)
+        vimrc_home.symlink_to(vim)
         print('Enlace creado')
         print('Plantillas creadas')
 
-def plantillas(vim, ruta):
+def plantillas(vimrc, ruta):
     archivos = []
-    with open(vim, 'r', encoding='utf-8') as a:
+    with open(vimrc, 'r', encoding='utf-8') as a:
         contenido = a.read()
 
     for x in ruta.iterdir():
@@ -33,18 +33,18 @@ def plantillas(vim, ruta):
         if not 'vimrc' in str(x):
             comando = f'autocmd BufNewFile *{y} 0r {x}\n'
             if not comando in contenido:
-                with open(vim, 'a', encoding='utf-8') as a:
+                with open(vimrc, 'a', encoding='utf-8') as a:
                     a.write(comando)
 
-def comandos(vim):
+def comandos(vimrc):
     c_latex = f'autocmd BufNewFile,BufRead *.tex nnoremap <C-b> :w<CR> :silent !pdflatex % && rm -f %:r.log %:r.aux %:r.toc %:r.out<CR> :redraw!<CR>\n'
     c_c = f'autocmd BufNewFile,BufRead *.c nnoremap <C-b> :w<CR> :silent !gcc % -o %:r<CR> :redraw!<CR>\n'
     
-    with open(vim, 'r', encoding='utf-8') as a:
+    with open(vimrc, 'r', encoding='utf-8') as a:
         contenido = a.read()
 
     if not (c_latex and c_c) in contenido:
-        with open(vim, 'a', encoding='utf-8') as a:
+        with open(vimrc, 'a', encoding='utf-8') as a:
             a.write(c_latex)
             a.write(c_c)
 
